6026b66c7f680bc2a8a3c4cf0e4abf76ebe70f51
e <buguduotai@163.com>
2019-11-22 01:35:06 +0800

    发布问题

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -4,26 +4,53 @@
     hide-on-scroll
     fixed
     height="50"
-    style="border-top:4px solid #2196F3; color:#888888"
+    style="border-top:4px solid #1976d2; color:#888888"
   >
     <v-app-bar-nav-icon>
-      <v-icon color="#2196F3">mdi-tennis</v-icon>
+      <v-icon color="#1976d2" to="/home">mdi-tennis</v-icon>
     </v-app-bar-nav-icon>
-    <v-toolbar-title style="font-size:1em;">布谷社区</v-toolbar-title>
-
+    <v-toolbar-title style="font-size:1em; color:#777777">布谷社区</v-toolbar-title>
     <v-spacer></v-spacer>
-
     <v-toolbar-items>
+      <!-- 发布什么下拉框 -->
+      <div>
+        <v-menu open-on-hover offset-y>
+          <template v-slot:activator="{ on }">
+            <!-- <v-btn style="color:#777777; background:#fff; margin:5px 10px 0 0;" >
+                <v-icon>mdi-pencil</v-icon>
+            </v-btn>-->
+            <v-btn
+              style="color:#777777; border:none; background:#fff; margin:7px 5px 0 0;"
+              tile
+              outlined
+              color="#777777"
+              v-on="on"
+            >
+              <v-icon left>mdi-pencil</v-icon>发布
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in dropDownList"
+              :key="index"
+              @click="dropDown(item.title)"
+            >
+              <v-list-item-title style="font-size:0.8em; color:#777777;">{{item.title}}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+      <!-- 发布什么下拉框 end -->
+
       <v-btn style="color:#777777;" text to="/home">社区</v-btn>
-      <v-btn style="color:#777777;" text to="/a">文档</v-btn>
       <v-btn style="color:#777777;" text to="/b">招聘</v-btn>
       <v-btn style="color:#777777;" text to="/">博客</v-btn>
       <v-btn style="color:#777777;" text to="/inspire">友链</v-btn>
     </v-toolbar-items>
 
     <!-- 头像 -->
-    <v-avatar v-if="isLogin" size=35>
-      <img v-bind:src="user.avatarUrl" alt="" />
+    <v-avatar v-if="isLogin" size="35">
+      <img v-bind:src="user.avatarUrl" alt />
     </v-avatar>
     <!-- 登录按钮 -->
     <div class="text-center" v-if="!isLogin">
@@ -70,7 +97,6 @@
 import axios from "axios";
 import qs from "qs";
 import global from "../global";
-
 export default {
   data: () => ({
     dialog: false,
@@ -78,7 +104,12 @@ export default {
     user: {},
     isLogin: false,
     snackbar: false,
-    text: "登录成功！"
+    text: "登录成功！",
+    dropDownList: [
+      { title: "博客" },
+      { title: "提问" },
+      { title: "分享" }
+    ]
   }),
 
   created() {},
@@ -90,23 +121,26 @@ export default {
     //因为重定后设置的 cookie 没传到跳转的页面，所以这里从路径上获取 token，在调用查询数据库用户表接口时存到 cookie 中
     let token = this.$route.query.token;
     if (token != null || token != undefined) {
-      axios.get("/user/find", { params: { token } }).then(res => {
-        this.$router.push("/");
-        this.user = res.data.data;
-        this.isLogin = true;
-        this.snackbar = true;
-        console.log(this.user);
-      }).catch(error => {
-        this.snackbar = true;
-        this.color = "#4CAF50";
-      });
+      axios
+        .get("/user/find", { params: { token } })
+        .then(res => {
+          this.$router.push("/");
+          this.user = res.data.data;
+          this.isLogin = true;
+          this.snackbar = true;
+          console.log(this.user);
+        })
+        .catch(error => {
+          this.snackbar = true;
+          this.color = "#4CAF50";
+        });
     } else {
       //刷新时调用该接口，若 cookie 中已有 token，则直接获取数据
       axios.get("/user/refresh").then(res => {
         this.user = res.data.data;
         this.isLogin = true;
         console.log(res);
-      })
+      });
     }
   },
 
@@ -115,6 +149,11 @@ export default {
       console.log("进入了登录方法");
       window.location.href = global.apiUrl + "/oauth/login/gitee";
       // window.location.href = "http://192.168.0.102:3000"
+    },
+    dropDown(e){
+      if(e == "提问"){
+        this.$router.push("/question");
+      }
     }
   }
 };
---
+++ pages/question.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container class="grey lighten-5">
+    <!-- no-gutters 无间隔 -->
+    <v-row justify="end">
+      <v-col cols="12" sm="6" md="9">
+        <v-card class="pa-2" outlined>
+          <h3 style="text-align:center; margin:40px; font-size:1.2em; color:#636b6f;">
+            编辑问题
+            <v-icon size="20">mdi-pencil</v-icon>
+          </h3>
+          <v-text-field style="margin:0 auto; width:90%;"
+            label="标题"
+            outlined
+            height="2"
+            clearable
+            dense
+          ></v-text-field>
+          <v-select style="margin:0 auto; width:90%;"
+            v-model="value"
+            :items="items"
+            chips
+            label="标签"
+            multiple
+            outlined
+            dense
+            deletable-chips
+          ></v-select>
+
+          <!-- <v-textarea style="margin:0 auto; width:90%;"
+          outlined
+          name="input-7-4"
+          label="请使用markdown编写"
+          value=""
+        ></v-textarea> -->
+        <editor/>
+        <v-btn style="margin: 30px 0 10px 41px" depressed color="primary"><v-icon>mdi-cloud-upload</v-icon>&nbsp;发布</v-btn>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" outlined>.col-6 .col-md-4</v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import editor from '~/components/common/QuillEditor.vue'
+export default {
+    components: {
+        editor
+    },
+    data: () => ({
+      items: ['foo', 'bar', 'fizz', 'buzz'],
+      value: ['foo', 'bar', 'fizz', 'buzz'],
+    }),
+}
+</script>
